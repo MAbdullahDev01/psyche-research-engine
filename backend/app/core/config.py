b2aa9dff6d7903e8ccdeea01d9d9dcd871f00b03
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-09-19 09:52:03 +0500

    feat(backend): add new environmental variable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PUBLIC_SUPABASE_URL: str
     PUBLIC_SUPABASE_PUBLISHABLE_KEY: str
+    SUPABASE_SERVICE_ROLE_KEY: str
     PUBLIC_CLERK_PUBLISHABLE_KEY: str
     CLERK_SECRET_KEY: str
     CLERK_WEBHOOK_SIGNING_SECRET: str
