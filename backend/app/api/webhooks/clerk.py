f25e6abcc6d7e75e26ff4b4fd4419325b8aedfb6
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-09-19 11:02:53 +0500

    feat(backend): handle user deletion

--- backend/app/api/webhooks/clerk.py
+++ backend/app/api/webhooks/clerk.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, status
 
 from app.core.security import verify_clerk_webhook
-from app.services.user_services import add_user
+from app.services.user_services import add_user, delete_user
 
 router = APIRouter(prefix="/api/webhooks", tags=["webhooks"])
 
@@ -25,7 +25,7 @@ async def clerk_webhook(request: Request, status_code=status.HTTP_200_OK):
         case "user.updated":
             ...
         case "user.deleted":
-            ...
+            delete_user(event)
         case _:
             status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
             return {status_code : f"Unhandled webhook event type: {event.get('type')}"}
--- backend/app/services/user_services.py
+++ backend/app/services/user_services.py
@@ -1,16 +1,29 @@
 from app.db.supabase import supabase
 
 def add_user(event):
-    
-    response = (
-    supabase.table("users")
-    .insert(
-        {
-            "clerk_id": event.get("data", {}).get("id"),
-            "first_name": event.get("data", {}).get("first_name"),
-            "last_name": event.get("data", {}).get("last_name"),
-            "image_url" : event.get("data", {}).get("image_url"),
-        }
+    try:
+        response = (
+            supabase.table("users")
+            .insert(
+                {
+                    "clerk_id": event.get("data", {}).get("id"),
+                    "first_name": event.get("data", {}).get("first_name"),
+                    "last_name": event.get("data", {}).get("last_name"),
+                    "image_url" : event.get("data", {}).get("image_url"),
+                }
+            )
+            .execute()
         )
-    .execute()
-)
+    except Exception as e:
+        print(f"Error adding user: {e}")
+
+def delete_user(event):
+    try:
+        response = (
+            supabase.table("users")
+            .delete()
+            .eq("clerk_id", event.get("data", {}).get("id"))
+            .execute()
+        )
+    except Exception as e:
+        print(f"Error deleting user: {e}")
